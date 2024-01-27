7b39d41a095fe5be7dd373c384546d55e3dea1d8
Hanke Chen <i@kokecacao.me>
2024-01-27 12:40:59 -0500

    :sparkles: add player movement

--- Assets/Scripts/Character/PlayerMovement.cs
+++ Assets/Scripts/Character/PlayerMovement.cs
@@ -6,160 +6,102 @@ using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Config config;
-    public GameObject camera_Object;
-    // rigidbody
-    Rigidbody rgbd;
+  // components
+  Config config;
+  public GameObject cameraObject;
+  Rigidbody rgbd;
 
-    // factors
-    float jump_Speed;
-    float move_Speed_Max;
-    bool is_OnGround;
+  // constants
+  Vector3 RIGHT;
+  Vector3 FORWARD;
+  float JUMP_SPEED;
+  float MOVE_SPEED_MAX; // not used
 
-    float current_Speed;
+  // variables
+  bool isOnGround;
 
-    // directions
-    Vector3 right;
-    Vector3 forward;
-    Vector3 gravity;
+  // Start is called before the first frame update
+  void Start()
+  {
+    // initialize components
+    config = FindAnyObjectByType<Config>();
+    rgbd = GetComponent<Rigidbody>();
 
-    Vector3 moveDirection;
+    // set constants
+    JUMP_SPEED = config.playerJumpSpeed;
+    MOVE_SPEED_MAX = config.playerMoveSpeedMax;
+    RIGHT = cameraObject.transform.right;
+    Vector3 temp = cameraObject.transform.forward;
+    FORWARD = new Vector3(temp.x, 0, temp.z);
+    FORWARD.Normalize();
 
+    // set status
+    isOnGround = true;
+  }
 
-    
-
-    // Start is called before the first frame update
-    void Start()
+  private void OnCollisionStay(Collision collision)
+  {
+    if (collision.collider.gameObject.CompareTag("Floor"))
     {
-        config = FindAnyObjectByType<Config>();
-        rgbd = GetComponent<Rigidbody>();
-
-        // set values
-        jump_Speed = config.playerJumpSpeed;
-        move_Speed_Max = config.playerMoveSpeedMax;
-
-        // set directions
-        right = camera_Object.transform.right;
-        Vector3 temp = camera_Object.transform.forward;
-        forward = new Vector3(temp.x, 0, temp.z);
-        forward.Normalize();
-
-        moveDirection = Vector3.zero;
-
-        // set status
-        is_OnGround = true;
-
+      isOnGround = true;
     }
+  }
 
-    private void OnCollisionStay(Collision collision)
+  private void OnCollisionExit(Collision collision)
+  {
+    if (collision.collider.gameObject.CompareTag("Floor"))
     {
-        if (collision.collider.gameObject.CompareTag("Floor"))
-        {
-            is_OnGround = true;
-        }
+      isOnGround = false;
     }
+  }
 
-    private void OnCollisionExit(Collision collision)
+  private Vector3 getMovementInput()
+  {
+    Vector3 moveDirection = Vector3.zero;
+
+    # region keyboard input
+    if (Input.GetKey(KeyCode.A))
     {
-        if (collision.collider.gameObject.CompareTag("Floor"))
-        {
-            is_OnGround = false;
-        }
+      moveDirection -= RIGHT;
+    }
+    else if (Input.GetKey(KeyCode.D))
+    {
+      moveDirection += RIGHT;
+    }
+    else if (Input.GetKey(KeyCode.W))
+    {
+      moveDirection += FORWARD;
+    }
+    else if (Input.GetKey(KeyCode.S))
+    {
+      moveDirection -= FORWARD;
+    }
+    #endregion
+
+    #region gamepad input
+    float horizontal = Input.GetAxis("Horizontal");
+    float vertical = Input.GetAxis("Vertical");
+    moveDirection = FORWARD * vertical + RIGHT * horizontal;
+    #endregion
+
+    moveDirection.Normalize();
+    return moveDirection;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (isOnGround && Input.GetKeyDown(KeyCode.Space))
+    {
+      rgbd.AddForce(Vector3.up * JUMP_SPEED, ForceMode.Impulse);
+      // TODO: add gamepad jump
     }
 
-    // Update is called once per frame
-    void Update()
-    {        
-        if (is_OnGround)
-        {
-            moveDirection = Vector3.zero;
+    Vector3 moveDirection = getMovementInput();
+    Debug.Log(moveDirection);
 
-            // jump
-            if (Input.GetKeyDown(KeyCode.Space))
-                rgbd.AddForce(Vector3.up * jump_Speed, ForceMode.Impulse);
-
-            #region gamepad input
-            /*
-            float horizontal = Input.GetAxis("Horizontal");
-            float vertical = Input.GetAxis("Vertical");
-            moveDirection = forward * vertical + right * horizontal;
-            */
-            #endregion
-        }
-
-        // move
-        #region keyboard input
-        bool isInputAD = true;
-        bool isInputWS = true;
-        // move
-        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
-        {
-            moveDirection -= right;
-        }
-
-        else if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
-        {
-            moveDirection += right;
-        }
-
-        else isInputAD = false;
-
-
-        if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
-        {
-            moveDirection += forward;
-        }
-
-
-        else if (Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W))
-        {
-            moveDirection -= forward;
-        }
-
-        else isInputWS = false;
-
-        // edge case: acceleration out of range
-        if (current_Speed >= move_Speed_Max)
-        {
-            current_Speed = move_Speed_Max;
-        }
-
-        if (!isInputAD && !isInputWS)
-        {
-
-        }
-        #endregion
-
-        moveDirection.Normalize();
-        this.transform.position += moveDirection * move_Speed_Max * Time.deltaTime;
-
-        // click to emit a ray
-        if (Input.GetMouseButtonDown(0))
-        {
-            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-
-            RaycastHit[] rayHits = Physics.RaycastAll(mousePosition, camera_Object.transform.forward, 200f);
-
-            
-
-            foreach(RaycastHit rayHit in rayHits)
-            {
-                if (rayHit.collider.gameObject != null && rayHit.collider.gameObject.CompareTag("Portable"))
-                {
-                    GameObject target = rayHit.collider.gameObject;
-                    Portable portable = target.GetComponent<Portable>();
-
-                    Vector3 positionTemp = target.transform.position;
-                    portable.onSelected.Invoke(this.transform.position, moveDirection, rgbd.velocity);
-                    this.gameObject.transform.position = positionTemp;
-                }
-            }
-
-        }       
-
-    }
-
-   
-
+    // add force to player if move speed isn't at max
+    if (rgbd.velocity.magnitude < config.playerMoveSpeedMax)
+      rgbd.AddForce(moveDirection, ForceMode.Impulse);
+  }
 }
